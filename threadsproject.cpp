bfc3ebaf2f2ec965b22d6897210aa1109b1708a9
jual6332@colorado.edu <jual6332@colorado.edu>
2022-03-18 16:48:03 -0400

    Added functionality to track gasfillups based on pump#

--- threadsproject.cpp
+++ threadsproject.cpp
@@ -19,6 +19,11 @@ class Car {
   }
 };
 
+class GasPump {
+  public:
+    int numberOfFillups = 0;
+};
+
 /*
 // Change this function to accept a Car object as an input parameter
 void exec(int n){
@@ -26,8 +31,9 @@ void exec(int n){
 }
 */
 
-void fillUpWithGas(Car *inputCar){
+void fillUpWithGas(Car *inputCar, GasPump *inputPump){
   std::this_thread::sleep_for(std::chrono::milliseconds(30));
+  inputPump->numberOfFillups++;
   inputCar->numberOfFillups++;
 }
   
@@ -50,18 +56,27 @@ int main() {
   arrayOfCars[2] = thirdCar;
   arrayOfCars[3] = fourthCar;
 
+  GasPump gasPump1;
+  GasPump gasPump2;
+
   int totalNumberOfGasFillupsAllCars = 0;
 
-  while (totalNumberOfGasFillupsAllCars < 20){
-    for (int i=0; i<4; i++){
-      fillUpWithGas(&arrayOfCars[i]);
-      std::cout << arrayOfCars[i].numberOfFillups << std::endl;
+  while (totalNumberOfGasFillupsAllCars < 1000){
+    for (int i=0; i<9; i++){
+      if (totalNumberOfGasFillupsAllCars % 2 == 0){
+        fillUpWithGas(&arrayOfCars[i],&gasPump1);
+      } else {
+        fillUpWithGas(&arrayOfCars[i],&gasPump2);
+      }
+      //std::cout << arrayOfCars[i].numberOfFillups << std::endl;
       totalNumberOfGasFillupsAllCars++;
     }
   }
 
   // Print Total Number of Times that a Pump was used to fillup a Car with Gas
   std::cout << "Total # of gas fillups: " << totalNumberOfGasFillupsAllCars << std::endl;
+  std::cout << "Total fillups at gasPump 1: " << gasPump1.numberOfFillups << std::endl;
+  std::cout << "Total fillups at gasPump 2: " << gasPump2.numberOfFillups << std::endl;
   
-  std::cout << "Hello World!\n";
+  std::cout << "Project Finished!\n";
 }
